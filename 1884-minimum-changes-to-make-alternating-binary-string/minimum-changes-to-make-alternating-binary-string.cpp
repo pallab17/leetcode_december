3b75e13f56aa0b50be6a35776b44d09cbed7b13d
Pallab Banerjee <91554568+pallab17@users.noreply.github.com>
2023-12-24 23:03:33 +0530

    Time: 3 ms (78.34%) | Memory: 7.1 MB (98.52%) - LeetSync

--- 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.cpp
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
     int minOperations(string s) {
-        int n = s.length();
-        
-        int suru_with_0 =0;
-        int suru_with_1=0;
-        
+        int n = s.length();       
+        int suru_with_0 =0;        
         for(int i=0;i<n;i++){
             if(i%2==0){ // even pos
-                if(s[i]=='0'){
-                    suru_with_1++;
-                }else{
+                if(s[i]=='1'){
                     suru_with_0++;
                 }
-                
-            }else{  // odd pos
+            } else {  // odd pos
                 if(s[i]=='0'){
                     suru_with_0++;
-                }else{
-                    suru_with_1++;
                 }
             }
         }
-        
+        int suru_with_1=n-suru_with_0;
         return min(suru_with_1,suru_with_0);
     }
 };
