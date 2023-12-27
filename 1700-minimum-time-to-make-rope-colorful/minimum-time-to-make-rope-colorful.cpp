1c736bf42ec5b9d8730bd17dded8b85558b4649f
Pallab Banerjee <91554568+pallab17@users.noreply.github.com>
2023-12-27 18:14:08 +0530

    Time: 95 ms (97.91%) | Memory: 95.7 MB (83.31%) - LeetSync

--- 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.cpp
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.cpp
@@ -5,12 +5,12 @@ public:
         int time=0;
         int agerBalloonPatharTime = 0;
         for(int i=0;i<n;i++){
-            if( i>0 && colors[i] != colors[i-1]){
+            if( i>0 && colors[i] != colors[i-1]){    //ager balloon aar hate je  ballon  same color r balloon noye 
                 agerBalloonPatharTime = 0;
             }
-            int currentBalloonPatharTime = neededTime[i];
-            time+= min(agerBalloonPatharTime,currentBalloonPatharTime);
-            agerBalloonPatharTime = max(agerBalloonPatharTime,currentBalloonPatharTime);
+            int currentBalloonPatharTime = neededTime[i];    // hate je balloon ta ache tar phatar time
+            time+= min(agerBalloonPatharTime,currentBalloonPatharTime); // ager aar hater baloon same color hole je taratari phatbe take age pathabo
+            agerBalloonPatharTime = max(agerBalloonPatharTime,currentBalloonPatharTime);  // ager aar hater baloon same color hole je taratari phatbe na take store kore rakhbo
         }
         return time;
     }
